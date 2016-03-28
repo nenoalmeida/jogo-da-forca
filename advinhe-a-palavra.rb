46a3fb889fbcb4ac6b55a821fde9ea8d7d742d7e
Neno Almeida <nenoxvx@icloud.com>
2016-03-28 16:31:00 -0300

    segundo commit
    Jogo da forca

---
+++ advinhe-a-palavra.rb
@@ -0,0 +1,48 @@
+novo_jogo = "s"
+
+	while novo_jogo == "s"
+
+	palavras = %w(cachorro gato camelo formiga morcego dromedario)
+	palavra_sorteada = palavras.sample 
+	palavra_tamanho = palavra_sorteada.size
+
+	puts "Descubra a palavra que eu estou pensando"
+
+	palavra_parcial = []
+	palavra_tamanho.times do 
+		palavra_parcial << " _ "
+	end	
+
+	puts palavra_parcial.join
+
+	fim = false
+
+	while fim == false
+		puts "Escolha uma letra: "
+		sua_letra = gets.chomp
+
+		aux = 0
+
+		palavra_sorteada.each_char do |letra_palavra|
+			if palavra_parcial[aux] == " _ "
+				if letra_palavra == sua_letra
+					palavra_parcial[aux] = sua_letra
+				end
+			end
+			aux += 1
+		end
+		
+		if palavra_parcial.join.count(" _") > 0
+			fim = false
+			puts palavra_parcial.join
+		else
+			fim = true
+		end
+	end
+
+	puts "Parabens, a palavra era #{palavra_parcial.join}!"
+	puts "deseja jogar novamente?? (s/n)"
+	novo_jogo = gets.chomp
+end
+
+puts "obrigado pro jogar"
